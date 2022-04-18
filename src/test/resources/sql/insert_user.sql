578526b32b7be29d33d1da92cc7c200e451988a0
ZERDICORP <nikolaianikin2002@gmail.com>
2022-04-18 06:57:37 +0300

    changed test email

--- src/test/resources/sql/insert_user.sql
+++ src/test/resources/sql/insert_user.sql
@@ -1,4 +1,4 @@
 TRUNCATE user;
 
-INSERT INTO user (email, password_hash) VALUES ("mock@mock.mock",
+INSERT INTO user (email, password_hash) VALUES ("test@gmail.com",
   "77aae185203edc6357676db95caa25d0f398d402c1723e6a7b42cfe8d2967f2e");
